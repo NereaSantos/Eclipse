985674ee023c67ea10a8d99cc193438541536ee2
nerea <santoscanalnerea@gmail.com>
2023-03-25 12:25:23 +0100

    Clase Fecha

---
+++ ClaseFecha/src/ClaseFecha/EsBisiesto.java
@@ -0,0 +1,15 @@
+package ClaseFecha;
+
+public class EsBisiesto extends Fecha {
+
+	private boolean EsBisiesto() {
+		
+		if(this.getMes() == 2 && this.getDias() == 28) {
+			
+		}
+		
+		return false;
+		
+	}
+	
+}
---
+++ ClaseFecha/src/ClaseFecha/Mes.java
@@ -0,0 +1,29 @@
+package ClaseFecha;
+
+public class Mes {
+
+	private int dia;
+	private String nombre;
+	
+	public int getDia() {
+		return dia;
+	}
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getDias() {
+		
+		Mes[] meses = new Mes[12];
+		
+		meses[0].nombre
+		
+	}
+	
+}
