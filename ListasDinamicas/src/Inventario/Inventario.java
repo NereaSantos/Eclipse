f43fcb1764b0673e22dc5391ae4c56d7371ce531
nerea <santoscanalnerea@gmail.com>
2023-05-11 17:45:23 +0200

    Inventario corregido

--- ListasDinamicas/src/Inventario/Inventario.java
+++ ListasDinamicas/src/Inventario/Inventario.java
@@ -1,93 +1,71 @@
 package Inventario;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Inventario {
+    
+    private static Map<String, Producto> productos = new HashMap<>();
 
-	/*Inventario de productos (stock)
-	 * Nivel1: 
-	 * Insertar producto
-	 * Aumentar stock
-	 * Disminuir stock
-	 * Eliminar producto
-	 * Mostrar inventario
-	 * 
-	 * Nivel2: 
-	 * Tipo de producto
-	 * Mostrar stock medio de cada tipo de producto*/
+    public void agregarProducto(Producto producto) {
+        productos.put(producto.getCodigo(), producto);
+    }
 
-	    private static ArrayList<Producto> productos = new ArrayList<>();
-	    
-	    void agregarProducto(Producto producto) {
-	        this.productos.add(producto);
-	    }
-	    
-	    public static void aumentarStock(String codigo, int cantidad) {
-	        for (Producto producto : productos) {
-	        	if (producto.getCodigo().equals(codigo)) {
-	                producto.setCantidad(producto.getCantidad() + cantidad);
-	                break;
-	        	}
-	        }
-	    }
-	    
-	    public static void disminuirStock(String codigo, int cantidad) {
-	        for (Producto producto : productos) {
-	            if (producto.getCodigo().equals(codigo)) {
-	            	int nuevoStock = producto.getCantidad() - cantidad;
-	                if (nuevoStock < 0) {
-	                    nuevoStock = 0;
-	                }
-	                producto.setCantidad(nuevoStock);
-	                break;
-	            }
-	            }
-	        }
-	    
-	    public static void eliminarProducto(String codigo) {
-	        for (int i = 0; i < productos.size(); i++) {
-	            if (productos.get(i).getCodigo().equals(codigo)) {
-	                productos.remove(i);
-	                break;
-	            }
-	        }
-	    }
-	    
-	    public static void mostrarInventario() {
-	        for (Producto producto : productos) {
-	            System.out.println("Nombre: " + producto.getNombre());
-	            System.out.println("Precio: " + producto.getPrecio());
-	            System.out.println("Cantidad: " + producto.getCantidad());
-	            System.out.println("Tipo: " + producto.getTipo());
-	            System.out.println();
-	        }
-	    }
-	    
-	    public static void mostrarStockMedioPorTipo() {
-	        HashMap<String, Integer> sumaStockPorTipo = new HashMap<>();
-	        HashMap<String, Integer> cantidadProductosPorTipo = new HashMap<>();
-	        
-	        // Iterar sobre los productos y sumar su cantidad al valor correspondiente en los HashMaps
-	        for (Producto producto : productos) {
-	            String tipo = producto.getTipo();
-	            int cantidad = producto.getCantidad();
-	            
-	            if (sumaStockPorTipo.containsKey(tipo)) {
-	                sumaStockPorTipo.put(tipo, sumaStockPorTipo.get(tipo) + cantidad);
-	                cantidadProductosPorTipo.put(tipo, cantidadProductosPorTipo.get(tipo) + 1);
-	            } else {
-	                sumaStockPorTipo.put(tipo, cantidad);
-	                cantidadProductosPorTipo.put(tipo, 1);
-	            }
-	        }
-	        
-	        // Iterar sobre los HashMaps y calcular el stock medio de cada tipo de producto
-	        for (String tipo : sumaStockPorTipo.keySet()) {
-	            int sumaStock = sumaStockPorTipo.get(tipo);
-	            int cantidadProductos = cantidadProductosPorTipo.get(tipo);
-	            double stockMedio = (double) sumaStock / cantidadProductos;
-	            System.out.println("El stock medio de " + tipo + " es: " + stockMedio);
-	        }
-	    }
+    public static void aumentarStock(String codigo, int cantidad) {
+        if (productos.containsKey(codigo)) {
+            Producto producto = productos.get(codigo);
+            producto.setCantidad(producto.getCantidad() + cantidad);
+        }
+    }
+
+    public static void disminuirStock(String codigo, int cantidad) {
+        if (productos.containsKey(codigo)) {
+            Producto producto = productos.get(codigo);
+            int nuevoStock = producto.getCantidad() - cantidad;
+            if (nuevoStock < 0) {
+                nuevoStock = 0;
+            }
+            producto.setCantidad(nuevoStock);
+        }
+    }
+
+    public static void eliminarProducto(String codigo) {
+        productos.remove(codigo);
+    }
+
+    public static void mostrarInventario() {
+        for (Map.Entry<String, Producto> entry : productos.entrySet()) {
+            Producto producto = entry.getValue();
+            System.out.println("Nombre: " + producto.getNombre()
+            + " Precio: " + producto.getPrecio() + " Cantidad: " 
+            + producto.getCantidad() + " Tipo: " + producto.getTipo());
+ 
+        }
+    }
+
+    public static void mostrarStockMedioPorTipo() {
+        Map<String, Integer> sumaStockPorTipo = new HashMap<>();
+        Map<String, Integer> cantidadProductosPorTipo = new HashMap<>();
+
+        for (Map.Entry<String, Producto> entry : productos.entrySet()) {
+            Producto producto = entry.getValue();
+            String tipo = producto.getTipo();
+            int cantidad = producto.getCantidad();
+            
+            if (sumaStockPorTipo.containsKey(tipo)) {
+                sumaStockPorTipo.put(tipo, sumaStockPorTipo.get(tipo) + cantidad);
+                cantidadProductosPorTipo.put(tipo, cantidadProductosPorTipo.get(tipo) + 1);
+            } else {
+                sumaStockPorTipo.put(tipo, cantidad);
+                cantidadProductosPorTipo.put(tipo, 1);
+            }
+        }
+        
+        for (String tipo : sumaStockPorTipo.keySet()) {
+            int sumaStock = sumaStockPorTipo.get(tipo);
+            int cantidadProductos = cantidadProductosPorTipo.get(tipo);
+            double stockMedio = (double) sumaStock / cantidadProductos;
+            System.out.println("El stock medio de " + tipo + " es: " + stockMedio);
+        }
+    }
 }
--- ListasDinamicas/src/Inventario/PruebaInventario.java
+++ ListasDinamicas/src/Inventario/PruebaInventario.java
@@ -8,50 +8,41 @@ public class PruebaInventario {
 		 
 		 Producto producto1 = new Producto("Camiseta","A1", 23, 10, "ropa");
 	     Producto producto2 = new Producto("Pantalón", "A2", 46, 5, "ropa");
+	     
+	     Producto producto3 = new Producto("Cereal","B1", 5, 40, "comida");
+	     Producto producto4 = new Producto("Fruta", "B2", 3, 22, "comida");
     	
 	     
 	    inventario.agregarProducto(producto1);
 	    inventario.agregarProducto(producto2);
+	    inventario.agregarProducto(producto3);
+	    inventario.agregarProducto(producto4);
+	    
+	    System.out.println("\n Inventario Recién añadido \n");
 	    
 	    Inventario.mostrarInventario();
-	   
-	System.out.println("------------------------------------------");
-	    
-		/*Devuelve
-	     * Nombre: Camiseta
-			Precio: 23.0
-			Cantidad: 15
-			Tipo: ropa
-			
-			Nombre: Pantalon
-			Precio: 46.0
-			Cantidad: 3
-			Tipo: ropa*/
+	
+		System.out.println("\n Inventario despues de aumentar el stock de un producto y disminuir el de otro");
 	
 	    System.out.println("\n");
 	    Inventario.aumentarStock("A1", 5);
-	    Inventario.disminuirStock("A2", 2);
+	    Inventario.disminuirStock("A1", 16);
+	    Inventario.aumentarStock("B1", 10);
+	    Inventario.disminuirStock("B2", 6);
 
 	    Inventario.mostrarInventario();
-	   
-	 System.out.println("-----------------------------------------");
-	    
-	 /*Nombre: Pantalon
-			Precio: 46.0
-			Cantidad: 3
-			Tipo: ropa*/
+	 
+	 	System.out.println("\n Iventario despues de eliminar un producto");
 	 
 	    System.out.println("\n");
 	    Inventario.eliminarProducto("A1");
+	    Inventario.eliminarProducto("B2");
 
 	    Inventario.mostrarInventario();
-	    
-	 System.out.println("-----------------------------------------");  
-
-	 /*El stock medio de ropa es: 5.5*/
 	 
-	   System.out.println("\n");
-	    inventario.agregarProducto(new Producto("Calcetines", "A3",789, 8, "ropa"));
+	 	System.out.println("\n Mostrar la media de stock");
+	 
+	 	System.out.println("\n");
 
 	    Inventario.mostrarStockMedioPorTipo();
 	    
